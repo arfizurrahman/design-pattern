bf475b2eedd3c25f45a6e0b6db5bbe46ac0d0ae9
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-04-26 20:34:59 +0600

    Compeleted signleton exercises

---
+++ DesignPatternsC#/DesignPatterns/MonoState/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonoState
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceo = new CEO();
+            ceo.Name = "Arfiz";
+            ceo.Age = 25;
+
+            var ceo2 = new CEO();
+            Console.WriteLine(ceo2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatternsC#/Singleton/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MoreLinq;
+using NUnit.Framework;
+
+namespace Singleton
+{
+    public interface IDatabase
+    {
+        int GetPopulation(string name);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+        private static int instanceCount;
+        public static int Count => instanceCount;
+
+        private SingletonDatabase()
+        {
+            instanceCount++;
+            Console.WriteLine("Initialize Database");
+            //capitals = File.ReadAllLines("capitals.txt")
+            capitals = File.ReadAllLines(Path.Combine(new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName, "capitals.txt"))
+                .Batch(2)
+                .ToDictionary(list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+
+        private static Lazy<SingletonDatabase> instance = 
+            new Lazy<SingletonDatabase>(() => new SingletonDatabase());
+
+        public static SingletonDatabase Instance => instance.Value;
+    }
+
+    [TestFixture]
+    public class SingletonTests
+    {
+        [Test]
+        public void IsSingletonTest()
+        {
+            var db = SingletonDatabase.Instance;
+            var db2 = SingletonDatabase.Instance;
+            Assert.That(db, Is.SameAs(db2));
+            Assert.That(SingletonDatabase.Count, Is.EqualTo(1));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDatabase.Instance;
+            Console.WriteLine(db.GetPopulation("Tokyo"));
+        }
+    }
+}
